9ffb5c10198fa7d2fd5e6810400c8d28bf89fb5e
Shital Savekar <iamshital@users.noreply.github.com>
2017-11-02 18:46:36 +0530

    Update script1.ps1

--- scripts/script1.ps1
+++ scripts/script1.ps1
@@ -6,6 +6,6 @@ Write-Host $PWD
 Write-Host ----------------------
 echo ".\AzureAutomationManager.ps1 -runtests -Distro U16IP1CDK -cycleName PROVISION -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager"
 Write-Host ----------------------
-.\AzureAutomationManager.ps1 -runtests -Distro SS1604EHPC -cycleName PROVISION -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager
+.\AzureAutomationManager.ps1 -runtests -Distro U16IP1CDK -cycleName PROVISION -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager
 sleep 1800
 exit 0
