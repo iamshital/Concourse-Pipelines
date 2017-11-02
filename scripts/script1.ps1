b2230a535a0a4ecaf75b4054781b2c55247afde6
Shital Savekar <iamshital@users.noreply.github.com>
2017-11-02 19:53:21 +0530

    Update script1.ps1

--- scripts/script1.ps1
+++ scripts/script1.ps1
@@ -6,19 +6,12 @@ Write-Host $PWD
 Write-Host ----------------------
 echo ".\AzureAutomationManager.ps1 -runtests -Distro U16IP1CDK -cycleName PROVISION -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager"
 Write-Host ----------------------
-<#
 $Day = Get-Date -Format "yyyy-MM-dd"
 $Time = Get-Date -Format "HH-ss-ffff"
-
 mkdir C:\AutomationExecutions\$Day -ErrorAction SilentlyContinue
 mkdir C:\AutomationExecutions\$Day\$Time -ErrorAction SilentlyContinue
-
-
 $folderName = "C:\AutomationExecutions\$Day\$Time"
-
 Copy-Item -Recurse * $folderName
-
 cd $folderName
-#>
 .\AzureAutomationManager.ps1 -runtests -Distro U16IP1CDK -cycleName PROVISION -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager
 exit 0
