88f49de8aa6a55fec71da94bca44f2faa4c87234
Shital Savekar <iamshital@users.noreply.github.com>
2017-11-02 18:23:30 +0530

    Update script1.ps1

--- scripts/script1.ps1
+++ scripts/script1.ps1
@@ -1,2 +1,5 @@
-Copy-Item -Recurse -Verbose C:\concourse-worker\common-files .
-dir -Recurse
+Copy-Item -Path C:\concourse-worker\common-files\* -Destination . -Recurse -Force
+Get-ChildItem -Recurse | Unblock-File
+.\tools\AddAzureRmAccount.ps1
+.\AzureAutomationManager.ps1 -runtests -Distro SS1604EHPC -cycleName PROVISION -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager
+exit 0
