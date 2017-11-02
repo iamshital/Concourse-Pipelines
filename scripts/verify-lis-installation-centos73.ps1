2793ec78b3185a06dc397c49857c70a3d681a1cd
Shital Savekar <iamshital@users.noreply.github.com>
2017-11-02 20:27:30 +0530

    test

---
+++ scripts/verify-lis-installation-centos73.ps1
@@ -0,0 +1,17 @@
+Copy-Item -Path C:\concourse-worker\common-files\* -Destination . -Recurse -Force
+Get-ChildItem -Recurse | Unblock-File
+.\tools\AddAzureRmAccount.ps1
+Write-Host ----------------------
+Write-Host $PWD
+Write-Host ----------------------
+echo ".\AzureAutomationManager.ps1 -runtests -Distro CentOS73 -cycleName LIS-BUILD -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager -customLIS lisnext -customLISBranch master"
+Write-Host ----------------------
+$Day = Get-Date -Format "yyyy-MM-dd"
+$Time = Get-Date -Format "HH-ss-ffff"
+mkdir C:\AutomationExecutions\$Day -ErrorAction SilentlyContinue
+mkdir C:\AutomationExecutions\$Day\$Time -ErrorAction SilentlyContinue
+$folderName = "C:\AutomationExecutions\$Day\$Time"
+Copy-Item -Recurse * $folderName
+cd $folderName
+.\AzureAutomationManager.ps1 -runtests -Distro CentOS73 -cycleName LIS-BUILD -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager -customLIS lisnext -customLISBranch master
+exit 0
