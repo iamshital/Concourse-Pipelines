c09090bb34eccccb6433e8ae909207d5bcbf2ed5
Shital Savekar <iamshital@users.noreply.github.com>
2017-11-07 21:53:08 +0530

    Update verify-lis-installation-centos70.ps1

--- scripts/verify-lis-installation-centos70.ps1
+++ scripts/verify-lis-installation-centos70.ps1
@@ -4,7 +4,7 @@ Get-ChildItem -Recurse | Unblock-File
 Write-Host ----------------------
 Write-Host $PWD
 Write-Host ----------------------
-echo ".\AzureAutomationManager.ps1 -runtests -Distro CentOS70 -cycleName PROVISION -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager"
+echo ".\AzureAutomationManager.ps1 -runtests -Distro CentOS70 -cycleName LIS-BUILD -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager"
 Write-Host ----------------------
 $Day = Get-Date -Format "yyyy-MM-dd"
 $Time = Get-Date -Format "HH-ss-ffff"
