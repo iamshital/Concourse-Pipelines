3544a4348451eaecfbea2db5a21dff1fa5dd3b25
Shital Savekar <iamshital@users.noreply.github.com>
2017-11-07 21:44:38 +0530

    Update verify-lis-installation-centos73.ps1

--- scripts/verify-lis-installation-centos73.ps1
+++ scripts/verify-lis-installation-centos73.ps1
@@ -4,7 +4,7 @@ Get-ChildItem -Recurse | Unblock-File
 Write-Host ----------------------
 Write-Host $PWD
 Write-Host ----------------------
-echo ".\AzureAutomationManager.ps1 -runtests -Distro CentOS73 -cycleName PROVISION -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager"
+echo ".\AzureAutomationManager.ps1 -runtests -Distro CentOS73 -cycleName LIS-BUILD -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager"
 Write-Host ----------------------
 $Day = Get-Date -Format "yyyy-MM-dd"
 $Time = Get-Date -Format "HH-ss-ffff"
@@ -13,5 +13,5 @@ mkdir C:\AutomationExecutions\$Day\$Time -ErrorAction SilentlyContinue
 $folderName = "C:\AutomationExecutions\$Day\$Time"
 Copy-Item -Recurse * $folderName
 cd $folderName
-.\AzureAutomationManager.ps1 -runtests -Distro CentOS73 -cycleName PROVISION -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager
+.\AzureAutomationManager.ps1 -runtests -Distro CentOS73 -cycleName LIS-BUILD -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A1 -ImageType Standard -UseAzureResourceManager
 exit 0
